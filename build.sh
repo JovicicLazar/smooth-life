3ba029f0dc6a1c37bb64bf975060e0b09f7e30a1
Lazar Jovičić <92447936+JovicicLazar@users.noreply.github.com>
2023-07-21 15:42:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-g++ -O3 -Wall -Wextra ./src/settings_term.cpp  main.cpp -o smooth_life -ljsoncpp -lraylib
+g++ -O3 -Wall -Wextra ./src/settings_term.cpp  main.cpp -o smooth_life -ljsoncpp
 
 
