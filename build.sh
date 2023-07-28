75020b2708c59db34cf5386766453c1ba27bbf12
JovicicLazar <lazarjovicic808@gmail.com>
2023-07-28 14:56:40 +0200

    porting to gpu

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
-g++ -O3 -Wall -Wextra ./src/settings_term.cpp  main.cpp -o smooth_life -ljsoncpp
+g++ -O3 -Wall -Wextra ./src/settings_term.cpp  main.cpp -o smooth_life_term -ljsoncpp -lraylib
+
+g++ smooth_term.cpp -o smooth_life_gpu -lraylib
+
 
 
